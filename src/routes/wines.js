c9be79a49a567cf741a39a40da1d03de0bd169fa
Marius Žalkauskas <ma.zalkauskas@gmail.com>
2022-05-23 23:19:51 +0300

    wines post added

--- src/routes/wines.js
+++ src/routes/wines.js
@@ -8,14 +8,34 @@ const router = express.Router();
 router.get('/', isLoggedIn, async (req, res) => {
   try {
     const con = await mySQL.createConnection(mySQLConfig);
-    const [data] = await con.execute(`
-        SELECT * FROM wines
-          `);
+    const [data] = await con.execute('SELECT * FROM wines');
     await con.end();
 
     return res.send(data);
   } catch (err) {
-    return res.status(500).send({ err: 'Server problem' });
+    return res.status(500).send({ err: 'Server issue occured. Please try again' });
+  }
+});
+
+router.post('/', isLoggedIn, async (req, res) => {
+  try {
+    const con = await mySQL.createConnection(mySQLConfig);
+    const [data] = await con.execute(`
+    INSERT INTO wines (title, region, year)
+    VALUES (
+        ${mySQL.escape(req.body.title)},
+        ${mySQL.escape(req.body.region)},
+        ${mySQL.escape(req.body.year)}
+    )`);
+
+    if (!data.insertId) {
+      return res.status(500).send({ err: 'Server issue occured. Please try again' });
+    }
+
+    await con.end();
+    return res.send({ msg: 'Succesfully added a wine' });
+  } catch (err) {
+    return res.status(500).send({ err: 'Server issue occured. Please try again' });
   }
 });
 
