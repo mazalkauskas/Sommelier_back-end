af93366b323c066dea3fef6839efe5e6f45a0417
Marius Žalkauskas <ma.zalkauskas@gmail.com>
2022-05-23 21:08:34 +0300

    Config added

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { serverPort } = require('./config');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Server is running' });
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send({ err: 'Page not found' });
+});
+
+app.listen(serverPort, () => {
+  console.log(`Server is running on port ${serverPort}`);
+});
---
+++ src/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  serverPort: process.env.SERVER_PORT || 8080,
+  mySQLConfig: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT || 3306,
+  },
+  jwtSecret: process.env.JWT_SECRET,
+};
